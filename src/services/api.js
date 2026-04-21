8d95b5e3e59d2fd80f7dd05e8a42ed752a6a291e
Ganesh K <kkganesh2005@gmail.com>
2026-04-21 09:35:00 +0530

    update changes

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,46 @@
 import axios from "axios";
 
-Base_URL = https://t4e-testserver.onrender.com/api
+const BASE_URL = "https://t4e-testserver.onrender.com/api";
+
+let dataset = [];
 
 export const getToken = async () => {
-  const res = await axios.post("/public/token", {
-    studentId: "E0323029",
-    set: "setA",
-    password: "626448"
-  });
-  return res.data.token;
+try{
+const response = await axios.post(`${BASE_URL}/public/token`, {
+"studentId": "E0323029",
+"set": "setA",
+"password" : "626448"
+});
+
+console.log(response.data)
+const token = response.data.token;
+const dataUrl = response.data.dataUrl;
+
+const response2 = await axios.get(`${BASE_URL}${dataUrl}`, {
+headers: { "Authorization": `Bearer ${token}` },
+});
+
+dataset = response2.data.data;
+
+app.listen(PORT, () => {
+console.log(`Server is running ${PORT}`);
+});
+} catch(err){
+console.log(err);
+}
 };
+
+app.get("/" , (req,res) => {
+res.json({
+messeage : "Server is running "
+})
+});
+
+app.get("/employees" , (req,res) => {
+res.json({
+data : dataset
+})
+});
 
 export const getOrders = async (token) => {
   const res = await axios.get("/private/data", {
